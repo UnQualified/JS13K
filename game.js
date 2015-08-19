ec48c2bf21d0272f1838e744ea2082665c09d50c
unqualified (Android) <unqualified@outlook.com>
2015-08-19 14:35:29 +0100

    Initial commit from android tablet

--- game.js
+++ game.js
@@ -1,6 +1,30 @@
 function game() {
-  var canvas = document.getElementById('canvas');
-  // resize the canvas
-  canvas.width = 800;
-  canvas.height = 400;
+  
+  var game = new Core();
+  game.init();
 }
+
+function Core() {
+	this.canvas = document.getElementById('canvas');
+	this.ctx = this.canvas.getContext('2d');
+	
+	// resize the canvas
+	this.canvas.width = 800;
+	this.canvas.height = 400;
+}
+
+Core.prototype.init = function() {
+	window.requestAnimationFrame(this.draw);
+};
+
+Core.prototype.draw = function() {
+	this.ctx.font = "22px sans-serif";
+	this.ctx.fillText(this.getLetter(), 10, 50);
+	window.requestAnimationFrame();
+};
+
+Core.prototype.getLetter = function() {
+	var MIN = 65;
+	var MAX = 90;
+	return String.fromCharCode(MIN).toLowerCase();
+};
